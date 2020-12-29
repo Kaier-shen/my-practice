2bf32f1038ff415545fccb89bc75c653a6c29bfc
沈国家 <shenguojia@shenguojiadeMacBook-Pro.local>
2020-12-29 17:51:16 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <el-input></el-input> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+// import * as THREE from "three";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  mounted() {
+    // this.init();
+  },
+  methods: {
+    // init() {
+    //   const scene = new THREE.Scene();
+    //   const camera = new THREE.PerspectiveCamera(
+    //     75,
+    //     window.innerWidth / window.innerHeight,
+    //     0.1,
+    //     1000
+    //   );
+    //   const renderer = new THREE.WebGLRenderer();
+    //   renderer.setSize(window.innerWidth, window.innerHeight);
+    //   document.querySelector(".home").appendChild(renderer.domElement);
+    //   const geometry = new THREE.BoxGeometry();
+    //   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    //   const cube = new THREE.Mesh(geometry, material);
+    //   scene.add(cube);
+    //   camera.position.z = 5;
+    //   const animate = function() {
+    //     requestAnimationFrame(animate);
+    //     cube.rotation.x += 0.01;
+    //     cube.rotation.y += 0.01;
+    //     renderer.render(scene, camera);
+    //   };
+    //   animate();
+    // }
   }
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  height: 1000px;
+  width: 1000px;
+}
+</style>

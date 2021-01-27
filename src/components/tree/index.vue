4991d53061963cecf138f48b4bca1a995733558f
沈国家 <shenguojia@shenguojiadeMacBook-Pro.local>
2021-01-27 18:21:17 +0800

    t

--- src/components/tree/index.vue
+++ src/components/tree/index.vue
@@ -13,15 +13,9 @@
       @node-click="clickNode"
     >
       <span slot-scope="{ data }">
-        <!-- <span v-if="!filterText"> {{ data[defaultProps.label] }}</span> -->
         <span
           class="el-tree-node__label"
-          v-html="
-            data[defaultProps.label].replace(
-              new RegExp(`(${filterText.replace(/([/*.\\])/g, '\\$1')})`, 'ig'),
-              `<span style='color: #fc652f'>$1</span>`
-            )
-          "
+          v-html="getHtml(data[defaultProps.label], filterText)"
         />
       </span>
     </el-tree>
@@ -100,38 +94,48 @@ export default {
       this.$refs.tree.filter(v);
     },
     clickNode(data) {
+      console.log(data);
       this.$emit('clickNode', data);
+    },
+    getHtml(v, filterText) {
+      if (filterText === '') {
+        return v;
+      }
+      return v.replace(
+        new RegExp(`(${filterText.replace(/([/*.\\])/g, '\\$1')})`, 'ig'),
+        `<span style='color: #fc652f'>$1</span>`
+      );
     }
   }
 };
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .tree {
-  /deep/ .el-tree-node__content {
+  ::v-deep .el-tree-node__content {
     height: 30px;
     line-height: 30px;
     font-size: 12px;
   }
-  /deep/
+  ::v-deep
     .el-tree--highlight-current
     .el-tree-node.is-current
     > .el-tree-node__content {
     background: #f5f7fa;
     color: #fc652f;
   }
-  /deep/ .el-tree-node__expand-icon.expanded {
+  ::v-deep .el-tree-node__expand-icon.expanded {
     -webkit-transform: rotate(0deg);
     transform: rotate(0deg);
   }
-  /deep/ .el-icon-caret-right:before {
+  ::v-deep .el-icon-caret-right:before {
     font-size: 18px;
     content: '\e78a';
   }
-  /deep/ .el-tree-node__expand-icon.expanded.el-icon-caret-right:before {
+  ::v-deep .el-tree-node__expand-icon.expanded.el-icon-caret-right:before {
     font-size: 18px;
     content: '\e784';
   }
-  /deep/ .el-tree-node__content > .el-tree-node__expand-icon {
+  ::v-deep .el-tree-node__content > .el-tree-node__expand-icon {
     display: flex;
     align-items: center;
   }
